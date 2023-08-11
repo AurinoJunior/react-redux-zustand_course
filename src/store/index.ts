3dc4f156f0fd6c0facfbbd7405b517a8f77d2128
Aurino Geraldo <aurinoaj@gmail.com>
2023-08-11 11:00:42 -0300

    Create app layout

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,8 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 
-const todoSlice = createSlice({
-  name: "todo",
-  initialState: ["Pegar roupas na lavanderia"],
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
-export const { add } = todoSlice.actions;
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
 
 // Retorna o tipo da minha função store, logo volta o tipo do meu global state (store)
