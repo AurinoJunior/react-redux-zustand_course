9b31718f83681771d033482878d506f4245958f0
Aurino Geraldo <aurinoaj@gmail.com>
2023-08-14 14:32:28 -0300

    Create global hook

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,17 +1,12 @@
 import ReactPlayer from "react-player";
-import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
-import { next } from "../store/slices/player";
+
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
   const dispatch = useDispatch();
 
-  const lesson = useAppSelector((state) => {
-    const { currentLessonIndex, currentModuleIndex } = state.player;
-    return state.player.course.modules[currentModuleIndex].lessons[
-      currentLessonIndex
-    ];
-  });
+  const { currentLesson } = useCurrentLesson();
 
   function handleNextPlay() {
     dispatch(next());
@@ -24,7 +19,7 @@ export function Video() {
         height="100%"
         controls
         onEnded={handleNextPlay}
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   );
